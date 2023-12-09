8d1061ec83a3e29faa9645a775c912cc530c939d
sonamLama22 <lamadsonam19@gmail.com>
2023-12-08 19:21:31 -0500

    call to watchmode after omdb

--- index.js
+++ index.js
@@ -24,52 +24,91 @@ function request_handler(req, res) {
     main.pipe(res);
   } else if (req.url.startsWith("/search")) {
     let { movie } = url.parse(req.url, true).query;
-    get_info(movie, res);
+    get_movieInfo(movie, res);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end(`<h1>404 Not Found</h1>`);
   }
 }
 
-function get_info(movie, res) {
-  const omdb_endpoint = `https://www.omdbapi.com/?apikey=${omdb_apiKey}&t=${encodeURIComponent(
-    movie
-  )}`;
-  const omdb_request = https.request(omdb_endpoint, {
-    method: "GET",
-  });
-
-  omdb_request.once("response", process_stream);
-  function process_stream(movie_stream) {
-    let movie_data = "";
-    movie_stream.on("data", (chunk) => {
-      movie_data += chunk;
+function get_movieInfo(movie, res) {
+  //base case
+  if (movie.Response === "True") {
+    console.log("After omdb");
+    const imdbID = movie.imdbID;
+    // console.log(imdbID);
+    const watchmode_endpoint = `https://api.watchmode.com/v1/title/${imdbID}/sources?apiKey=${watchmode_apiKey}`;
+    const watchmode_request = https.request(watchmode_endpoint, {
+      method: "GET",
     });
-    movie_stream.on("end", () => serve_results(movie_data, res));
+    watchmode_request.once("response", (stream) =>
+      process_stream(stream, parse_streamingData, res)
+    );
+    watchmode_request.end();
+  } else {
+    console.log("1st call to OMDB");
+    const omdb_endpoint = `https://www.omdbapi.com/?apikey=${omdb_apiKey}&t=${encodeURIComponent(
+      movie
+    )}`;
+    const omdb_request = https.request(omdb_endpoint, {
+      method: "GET",
+    });
+
+    omdb_request.once("response", (stream) =>
+      process_stream(stream, parse_movieData, res)
+    );
+    omdb_request.end(); // sends response
   }
+}
 
-  function serve_results(movie_data, res) {
-    let movie_object = JSON.parse(movie_data);
-    let movie_name = movie_object.Title;
-    let movie_year = movie_object.Year;
-    let imdbID = movie_object.imdbID;
-    let movie_poster = movie_object.Poster;
-    let genre = movie_object.Genre;
+function process_stream(movie_stream, callback, res) {
+  let movie_data = "";
+  movie_stream.on("data", (chunk) => {
+    movie_data += chunk;
+  });
+  movie_stream.on("end", () => callback(movie_data, res));
+}
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(
-      `<img src="${movie_poster}"/>
+function parse_movieData(movie_data, res) {
+  let movie_object = JSON.parse(movie_data);
+  let movie_name = movie_object.Title;
+  let movie_year = movie_object.Year;
+  let imdbID = movie_object.imdbID;
+  let movie_poster = movie_object.Poster;
+  let genre = movie_object.Genre;
+  // console.log(movie_object.Response);
+
+  let results = `<img src="${movie_poster}"/>
         <h3> Movie Title: ${movie_name} 
         <br/> Year: ${movie_year}
         <br/> Genre: ${genre}
-        <br/> imdbID: ${imdbID} </h3> `
-    );
+        <br/> imdbID: ${imdbID} </h3> `;
+
+  results = `<div style="width:49%; float:left;">${results}</div>`;
+  res.write(results.padEnd(1024, " "));
+  get_movieInfo(movie_object, res); // recursion?
+
+  // function get_streamingInfo(imdbID,res);
+  // does res.end() take a callback fn?
+}
+
+function parse_streamingData(streaming_data, res) {
+  // console.log("received watchmode response");
+  let streaming_object = JSON.parse(streaming_data);
+  let results = "<h1>No results found</h1>";
+  // console.log(Object.keys(streaming_object).length);
+
+  if (Array.isArray(streaming_object)) {
+    let streaming_platform = streaming_object[0]?.name;
+    let url = streaming_object[0]?.web_url;
+    if (Object.keys(streaming_object).length === 0) {
+      results = "<h1>Not available for streaming</h1>";
+    } else {
+      results = `<h3> Available on: ${streaming_platform}</h3>
+    <a href="${url}"> Watch Now </a>`;
+    }
   }
-
-  omdb_request.end();
-
-  const watchmode_endpoint = `https://api.watchmode.com/v1/title/${imdbID}/sources?apiKey=${watchmode_apiKey}`;
-  const watchmode_request = https.request(watchmode_endpoint, {
-    method: "GET",
-  });
+  results = `<div style="width:49%; float:right;">${results}</div>`;
+  res.write(results.padEnd(1024, " "));
+  res.end();
 }
